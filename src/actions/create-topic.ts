8504b3a49251eec6d7ed51e1cdb4605b2a7ff028
Leo Uranga <leo.uranga@hotmail.com>
2025-12-22 02:20:59 -0500

    update

--- src/actions/create-topic.ts
+++ src/actions/create-topic.ts
@@ -1,6 +1,7 @@
 "use server";
 
 import { z } from "zod";
+import { auth } from "@/auth";
 
 const createTopicSchema = z.object({
   name: z
@@ -12,15 +13,41 @@ const createTopicSchema = z.object({
   description: z.string().min(10),
 });
 
-export async function createTopic(formData: FormData) {
+interface CreateTopicFormState {
+  errors: {
+    name?: string[];
+    description?: string[];
+    _form?: string[];
+  };
+}
+
+export async function createTopic(
+  formState: CreateTopicFormState,
+  formData: FormData
+): Promise<CreateTopicFormState> {
   const result = createTopicSchema.safeParse({
     name: formData.get("name"),
     description: formData.get("description"),
   });
 
   if (!result.success) {
-    console.log(result.error.flatten().fieldErrors);
+    return {
+      errors: result.error.flatten().fieldErrors,
+    };
   }
+
+  const session = await auth();
+  if (!session || !session.user) {
+    return {
+      errors: {
+        _form: ["You must be signed in to do this."],
+      },
+    };
+  }
+
+  return {
+    errors: {},
+  };
 
   //TODO: revalidade the homepage.
 }
--- src/components/topics/topic-create-form.tsx
+++ src/components/topics/topic-create-form.tsx
@@ -1,3 +1,7 @@
+"use client";
+
+import { useActionState, startTransition } from "react";
+
 import {
   Input,
   Button,
@@ -5,16 +9,31 @@ import {
   Popover,
   PopoverTrigger,
   PopoverContent,
+  Form,
 } from "@nextui-org/react";
 import * as actions from "@/actions";
+import { div } from "framer-motion/client";
+
 export default function TopicCreateForm() {
+  const [formState, action] = useActionState(actions.createTopic, {
+    errors: {},
+  });
+
+  function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+    const formData = new FormData(event.currentTarget);
+    startTransition(() => {
+      action(formData);
+    });
+  }
+
   return (
     <Popover placement="left">
       <PopoverTrigger>
         <Button color="primary">Create a Topic</Button>
       </PopoverTrigger>
       <PopoverContent>
-        <form action={actions.createTopic}>
+        <form onSubmit={handleSubmit} noValidate>
           <div className="flex flex-col gap-4 p-4 w-80">
             <h3 className="text-lg">Create a Topic</h3>
             <Input
@@ -22,13 +41,24 @@ export default function TopicCreateForm() {
               label="Name"
               labelPlacement="outside"
               placeholder="Name"
+              isInvalid={!!formState.errors.name}
+              errorMessage={formState.errors.name?.join(", ")}
             />
             <Textarea
               name="description"
               label="Description"
               labelPlacement="outside"
               placeholder="Describe your topic"
+              isInvalid={!!formState.errors.description}
+              errorMessage={formState.errors.description?.join(", ")}
             />
+
+            {formState.errors._form ? (
+              <div className=" rounded p-2 bg-red-200 border border-red-400">
+                {formState.errors._form?.join(", ")}
+              </div>
+            ) : null}
+
             <Button type="submit">Submit</Button>
           </div>
         </form>
